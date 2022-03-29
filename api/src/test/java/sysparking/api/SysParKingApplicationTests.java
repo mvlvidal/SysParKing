caf27b45e3d654bf53cf7166f625a5a5bb10f1d1
Marcus Vinicius Lima Vidal <marcus.vidal@prodeb.local>
2022-03-29 11:47:53 -0300

    projeto api

---
+++ api/src/test/java/sysparking/api/SysParKingApplicationTests.java
@@ -0,0 +1,13 @@
+package sysparking.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SysParKingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
